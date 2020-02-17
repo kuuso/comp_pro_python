88ab44a2db0ad2874e179245c4cea704b4b01f59
kuuso <kuuso1pc@gmail.com>
2020-02-18 01:33:25 +0900

    segtree: at(idx)/ segtree_with_index add

--- data_structure/segtree_with_index.py
+++ data_structure/segtree_with_index.py
@@ -3,7 +3,7 @@ sys.setrecursionlimit(100000)
 import random
 
 class seg_tree:
-    def __init__(self, n, op, id_ele):
+    def __init__(self, n, op, id_ele, left = True):
         self.N = n
         self.n = 1 # pow of 2
         while(self.n < self.N):
@@ -12,24 +12,36 @@ class seg_tree:
         self.id_ele = id_ele
         self.data = [self.id_ele] * (2 * self.n - 1)
 
+        self.left = left
+        self.index = [0] * (2 * self.n - 1)
+        for i in range(self.n): self.index[i + self.n - 1] = i
+        for i in reversed(range(self.n - 1)):
+            self.index[i] = self.index[i * 2 + 1] if left else self.index[i * 2 + 2]
+        
     def update(self, idx, v):
         idx += self.n - 1
         self.data[idx] = v
+        prefer = 0 if self.left else 1
         while idx > 0:
             idx = (idx - 1) // 2
             self.data[idx] = self.op(self.data[idx * 2 + 1], self.data[idx * 2 + 2])
+            self.index[idx] = self.index[idx * 2 + 1 + prefer] if self.data[idx] == self.data[idx * 2 + 1 + prefer] else self.index[idx * 2 + 1 + (1 - prefer)]
 
     def query(self, a, b):
         # op([a, b))
         return self._query(a, b, 0, 0, self.n)
 
     def _query(self, a, b, k, l, r):
-        if r <= a or b <= l: return self.id_ele
-        if a <= l and r <= b: return self.data[k]
+        prefer = self.N if self.left else -1
+        if r <= a or b <= l: return self.id_ele, prefer 
+        if a <= l and r <= b: return self.data[k], self.index[k]
 
-        vl = self._query(a, b, k * 2 + 1, l, (l + r) // 2)
-        vr = self._query(a, b, k * 2 + 2, (l + r) // 2, r)
-        return self.op(vl, vr)
+        vl, lidx = self._query(a, b, k * 2 + 1, l, (l + r) // 2)
+        vr, ridx = self._query(a, b, k * 2 + 2, (l + r) // 2, r)
+        ret = self.op(vl, vr)
+        if self.left: idx = lidx if vl == ret else ridx
+        else: idx = ridx if vr == ret else lidx
+        return ret, idx
 
     def queryAll(self):
         return self.data[0]
@@ -37,8 +49,10 @@ class seg_tree:
         return self.data[idx + self.n - 1]
     def initialize(self, a):
         for i in range(len(a)): self.data[i + self.n - 1] = a[i]
+        prefer = 0 if self.left else 1
         for i in reversed(range(self.n - 1)):
             self.data[i] = self.op(self.data[i * 2 + 1], self.data[i * 2 + 2])
+            self.index[i] = self.index[i * 2 + 1 + prefer] if self.data[i] == self.data[i * 2 + 1 + prefer] else self.index[i * 2 + 1 + (1 - prefer)]
         
     def dump(self):
         print()
@@ -51,6 +65,9 @@ class seg_tree:
                 cnt = 0
                 h += 1
                 print()
+        
+
+
 
 
 def main():
